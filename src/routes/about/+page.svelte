3ae7f2a5c0e2906debb357a596aa9d9652e21f57
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-11-03 15:29:39 +0000

    About and sources page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,4 +2,16 @@
 	<title>About</title>
 </svelte:head>
 
-about page
+<h1>About</h1>
+
+<h2>Technologies Used</h2>
+<ul>
+	<li>
+		<a href="https://kit.svelte.dev/">SvelteKit</a> was used to make routing easier and to componentize
+		everything
+	</li>
+	<li>
+		<a href="https://tailwindcss.com/">TailwindCSS</a> and <a href="https://sass-lang.com">SCSS</a> was
+		used to allow CSS to be written in HTML, and to allow a better DX
+	</li>
+</ul>
---
+++ src/routes/sources/+page.svelte
@@ -0,0 +1,3 @@
+<svelte:head>
+	<title>Sources</title>
+</svelte:head>
