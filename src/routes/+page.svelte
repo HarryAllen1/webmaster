1ac7b8e5020c5f7335e465f026cddabe550e62fa
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-11-03 20:48:52 -0700

    GEM!!!!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
+	import Gem from '../lib/Gem.svelte';
 	import HoverCard from '../lib/HoverCard.svelte';
 </script>
 
 <h1 class="text-blue">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <HoverCard />
+<Gem />
