7555542caf92078342eb0ef6a75affaadd27abde
lsakunes <senukasl@outlook.com>
2022-11-05 21:28:29 -0700

    made gif fade in

--- src/routes/suit/+page.svelte
+++ src/routes/suit/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import { fade } from 'svelte/transition';
+</script>
+
 <svelte:head>
 	<title>Suit</title>
 </svelte:head>
@@ -5,4 +9,8 @@
 <h1>The Suit</h1>
 
 <h2>MK V Gen 3</h2>
-<img src="/armor.gif" alt="Mark 5 Gen 3 spartan armor from Halo Infinite" />
+<img
+	in:fade={{ duration: 1000 }}
+	src="/armor.gif"
+	alt="Mark 5 Gen 3 spartan armor from Halo Infinite"
+/>
