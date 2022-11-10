f65ba8bee610901b911b0396cc75b907d2efe713
Senuka Liyanage <66855216+lsakunes@users.noreply.github.com>
2022-11-10 14:01:57 -0800

    feat: fix page size (#20)
    * added a suit page, along with a gif of a suit. It's place holder, in the final gif the floor won't move, only the suit.
    
    * added hardcoded size
    
    * format
    
    Co-authored-by: Harry Allen <66224939+HarryAllen1@users.noreply.github.com>

---
+++ src/routes/suit/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Suit from '$lib/Suit.svelte';
+</script>
+
+<svelte:head>
+	<title>Suit</title>
+</svelte:head>
+
+<h1>The Suit</h1>
+
+<h2>MK V Gen 3</h2>
+<img src="/armor.gif" alt="Mark 5 Gen 3 spartan armor from Halo Infinite" />
