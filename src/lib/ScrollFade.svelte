b5794d686b9c592e2df28e3286d81ab666192847
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-11-09 18:24:44 -0800

    Complete card

--- src/lib/ScrollFade.svelte
+++ src/lib/ScrollFade.svelte
@@ -7,11 +7,24 @@
 	export const textSide: 'left' | 'right' = 'left';
 
 	let containerEl: HTMLElement;
+	let animate = false;
 
-	onMount(async () => {});
+	onMount(async () => {
+		const observer = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					animate = true;
+				} else {
+					animate = false;
+				}
+			});
+		});
+
+		observer.observe(containerEl);
+	});
 </script>
 
-<div class="flex flex-row" bind:this={containerEl}>
+<div class:animate class="flex flex-row fade-card" bind:this={containerEl}>
 	{#if textSide === 'left'}
 		<slot name="text" />
 		<slot name="hero" />
@@ -20,3 +33,26 @@
 		<slot name="text" />
 	{/if}
 </div>
+
+<style lang="scss">
+	.fade-card {
+		opacity: 0;
+		filter: blur(5px);
+		transform: translateX(-20px);
+		transition: all 0.3s ease-in-out;
+		&.animate {
+			opacity: 1;
+			filter: blur(0);
+			transform: translateX(0);
+		}
+	}
+
+	@media (prefers-reduced-motion) {
+		.fade-card {
+			transition: none;
+			opacity: 1 !important;
+			filter: none !important;
+			transform: none !important;
+		}
+	}
+</style>
