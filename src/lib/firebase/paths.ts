71607071441cc64dcf3b41048167d0cb551aa5a5
lsakunes <senukasl@outlook.com>
2022-11-17 15:06:45 -0800

    fix: deps

--- src/lib/firebase/paths.ts
+++ src/lib/firebase/paths.ts
@@ -2,8 +2,8 @@ import { firestore } from './init';
 import { collection } from 'firebase/firestore';
 
 export enum Paths {
-  Applications = 'application',
-  Users = 'users',
+	Applications = 'application',
+	Users = 'users',
 }
 
 export const applicationsRef = collection(firestore, Paths.Applications);
