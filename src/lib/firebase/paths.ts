e0bf089129e34adbeed642c5556ddf812d3aec64
Harry Allen <66224939+HarryAllen1@users.noreply.github.com>
2022-11-17 15:03:21 -0800

    add some paths

---
+++ src/lib/firebase/paths.ts
@@ -0,0 +1,10 @@
+import { firestore } from './init';
+import { collection } from 'firebase/firestore';
+
+export enum Paths {
+  Applications = 'application',
+  Users = 'users',
+}
+
+export const applicationsRef = collection(firestore, Paths.Applications);
+export const usersRef = collection(firestore, Paths.Users);
