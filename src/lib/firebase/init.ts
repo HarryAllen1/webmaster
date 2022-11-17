ce740415e9e25d6fc1a6cecf0a81d9f5eff9b6c3
lsakunes <senukasl@outlook.com>
2022-11-17 15:06:45 -0800

    fix: deps

--- src/lib/firebase/init.ts
+++ src/lib/firebase/init.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const config = {
-  apiKey: 'AIzaSyAU8QumOx0drOpBQ7rmaMPpSJTYNcnGUBs',
-  authDomain: 'webmaster-tsa.firebaseapp.com',
-  projectId: 'webmaster-tsa',
-  storageBucket: 'webmaster-tsa.appspot.com',
-  messagingSenderId: '54073035471',
-  appId: '1:54073035471:web:5f40b4f3fe91b66f3085ba',
-  measurementId: 'G-8MSL7F4EHF',
+	apiKey: 'AIzaSyAU8QumOx0drOpBQ7rmaMPpSJTYNcnGUBs',
+	authDomain: 'webmaster-tsa.firebaseapp.com',
+	projectId: 'webmaster-tsa',
+	storageBucket: 'webmaster-tsa.appspot.com',
+	messagingSenderId: '54073035471',
+	appId: '1:54073035471:web:5f40b4f3fe91b66f3085ba',
+	measurementId: 'G-8MSL7F4EHF',
 };
 
 export const app = initializeApp(config);
