3c57ae8786e8128364d3c8ce560be8e0e232bd36
Harry Allen <66224939+HarryAllen1@users.noreply.github.com>
2023-03-02 08:45:20 -0800

    use named imports

--- models/renderer.mjs
+++ models/renderer.mjs
@@ -1,4 +1,13 @@
-import * as THREE from 'three';
+import {
+  AmbientLight,
+  BoxGeometry,
+  DirectionalLight,
+  Mesh,
+  MeshLambertMaterial,
+  OrthographicCamera,
+  Scene,
+  WebGLRenderer,
+} from 'three';
 
 customElements.define(
   'wm-models-page',
@@ -9,27 +18,27 @@ customElements.define(
         mode: 'open',
       });
       // Scene
-      const scene = new THREE.Scene();
+      const scene = new Scene();
 
       // Add a cube to the scene
-      const geometry = new THREE.BoxGeometry(3, 1, 3); // width, height, depth
-      const material = new THREE.MeshLambertMaterial({ color: 0xfb8e00 });
-      const mesh = new THREE.Mesh(geometry, material);
+      const geometry = new BoxGeometry(3, 1, 3); // width, height, depth
+      const material = new MeshLambertMaterial({ color: 0xfb8e00 });
+      const mesh = new Mesh(geometry, material);
       mesh.position.set(0, 0, 0);
       scene.add(mesh);
 
       // Set up lights
-      const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+      const ambientLight = new AmbientLight(0xffffff, 0.6);
       scene.add(ambientLight);
 
-      const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+      const directionalLight = new DirectionalLight(0xffffff, 0.6);
       directionalLight.position.set(10, 20, 0); // x, y, z
       scene.add(directionalLight);
 
       // Camera
       const width = 10;
       const height = width * (window.innerHeight / (window.innerWidth * 0.5));
-      const camera = new THREE.OrthographicCamera(
+      const camera = new OrthographicCamera(
         width / -2, // left
         width / 2, // right
         height / 2, // top
@@ -42,7 +51,7 @@ customElements.define(
       camera.lookAt(0, 0, 0);
 
       // Renderer
-      const renderer = new THREE.WebGLRenderer({ antialias: true });
+      const renderer = new WebGLRenderer({ antialias: true });
       renderer.setSize(window.innerWidth * 0.5, window.innerHeight);
       renderer.render(scene, camera);
 
