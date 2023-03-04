80f41c3140256519ce4415ac99abe7528d7dcf84
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-03-03 16:56:04 -0800

    add work log

--- about/work-log.mjs
+++ about/work-log.mjs
@@ -1,8 +1,272 @@
-customElements.define(
-  'wm-work-log',
-  class extends HTMLElement {
-    constructor() {
-      super();
-    }
-  }
-);
+export default [
+	{
+		author: "HarryAllen1",
+		message: "Merge branch 'main' of https://github.com/HarryAllen1/webmaster",
+		link: "https://github.com/HarryAllen1/webmaster/commit/ee0398036449acf273b98f143bdfd535af3f511a",
+	},
+	{
+		author: "HarryAllen1",
+		message: "use deno for everything for caching and stuff ig",
+		link: "https://github.com/HarryAllen1/webmaster/commit/2620a9d19216a8c93dda660127a216dc89ee1bb9",
+	},
+	{
+		author: "HarryAllen1",
+		message: "add stars to template",
+		link: "https://github.com/HarryAllen1/webmaster/commit/56b2ffd7d8599db381d18c266f621d4c392f1073",
+	},
+	{
+		author: "HarryAllen1",
+		message: "fix stars covering other elements",
+		link: "https://github.com/HarryAllen1/webmaster/commit/6e6c86bc78155179e371248caf7c2c38bb4c1119",
+	},
+	{
+		author: "HarryAllen1",
+		message: "fancy text",
+		link: "https://github.com/HarryAllen1/webmaster/commit/32bc48d2f20f7a73f2a66bd693edcca9013ada90",
+	},
+	{
+		author: "HarryAllen1",
+		message: "open in new tab",
+		link: "https://github.com/HarryAllen1/webmaster/commit/b1abd4381435e538422e3901ea06fea5f49ea7e4",
+	},
+	{
+		author: "HarryAllen1",
+		message: "um stars or something",
+		link: "https://github.com/HarryAllen1/webmaster/commit/2373904c8aa08fd6f1f1f52783ec747369f1cabe",
+	},
+	{
+		author: "HarryAllen1",
+		message: "Merge branch 'main' of https://github.com/harryallen1/webmaster",
+		link: "https://github.com/HarryAllen1/webmaster/commit/3a2e296103a81ab6c6b55aa8be87b2f6d4cffeca",
+	},
+	{
+		author: "HarryAllen1",
+		message: "Make navbar sticky,",
+		link: "https://github.com/HarryAllen1/webmaster/commit/0c042ff7749969bcd248d54f67c6a4df92d38f25",
+	},
+	{
+		author: "lsakunes",
+		message: "Merge branch 'main' of https://github.com/HarryAllen1/webmaster",
+		link: "https://github.com/HarryAllen1/webmaster/commit/9ba876790a4865f92aeea5411edf8e809790a60f",
+	},
+	{
+		author: "lsakunes",
+		message: "pkoewf",
+		link: "https://github.com/HarryAllen1/webmaster/commit/29efae773e87cd973c62fad8433cbcd858da5560",
+	},
+	{
+		author: "lsakunes",
+		message: "ronderer",
+		link: "https://github.com/HarryAllen1/webmaster/commit/5db90450d7e1bf3e0feacdab5b04363cb0af3db1",
+	},
+	{
+		author: "HarryAllen1",
+		message: "add some stars",
+		link: "https://github.com/HarryAllen1/webmaster/commit/910d762ee40778c8cf5131cb4a2ccc920901028a",
+	},
+	{
+		author: "HarryAllen1",
+		message: "add underline effect",
+		link: "https://github.com/HarryAllen1/webmaster/commit/b655edeba51e412dff94cc282d2b2c040e439a35",
+	},
+	{
+		author: "HarryAllen1",
+		message: "increase margin",
+		link: "https://github.com/HarryAllen1/webmaster/commit/1dae83487d01f3f414fd0d884679f41cd3c136e1",
+	},
+	{
+		author: "HarryAllen1",
+		message: "fade stuff in",
+		link: "https://github.com/HarryAllen1/webmaster/commit/638e4599727e352eb9ef644f1dd8e6ddea3cea17",
+	},
+	{
+		author: "HarryAllen1",
+		message: "fade in among ohter things",
+		link: "https://github.com/HarryAllen1/webmaster/commit/495cee8e3580637e1085dd899005b2b0c3700c5b",
+	},
+	{
+		author: "HarryAllen1",
+		message: "just make it look better",
+		link: "https://github.com/HarryAllen1/webmaster/commit/2dbd4b8bf322e9f3e1052f10bdd63b8346798d31",
+	},
+	{
+		author: "HarryAllen1",
+		message: "SENUKA WHY",
+		link: "https://github.com/HarryAllen1/webmaster/commit/ac9950b2a13c3335f88199b6196342dcff32b499",
+	},
+	{
+		author: "lsakunes",
+		message: "Merge branch 'main' of https://github.com/HarryAllen1/webmaster",
+		link: "https://github.com/HarryAllen1/webmaster/commit/e87a95a0f9bffe28e08848e2bf3de529177f4b96",
+	},
+	{
+		author: "HarryAllen1",
+		message: "use named imports",
+		link: "https://github.com/HarryAllen1/webmaster/commit/3c57ae8786e8128364d3c8ce560be8e0e232bd36",
+	},
+	{
+		author: "HarryAllen1",
+		message: "shadow root bc why not",
+		link: "https://github.com/HarryAllen1/webmaster/commit/561e253057e74c047da0ed7455495591db6c9afd",
+	},
+	{
+		author: "HarryAllen1",
+		message: "remove log",
+		link: "https://github.com/HarryAllen1/webmaster/commit/cbc9a016c9edcb34ce15ddd0ba959df288f1eeb0",
+	},
+	{
+		author: "HarryAllen1",
+		message: "fix width",
+		link: "https://github.com/HarryAllen1/webmaster/commit/874670a43e4dfcd8311bd01b591dab276e68b6ac",
+	},
+	{
+		author: "HarryAllen1",
+		message: "take advantage of custom elements",
+		link: "https://github.com/HarryAllen1/webmaster/commit/f86da228798eb6049354f68c3bcd04263d12da37",
+	},
+	{
+		author: "HarryAllen1",
+		message: "make stuff work in a really stupid way",
+		link: "https://github.com/HarryAllen1/webmaster/commit/8b8a433748d3d6c1b10d028e71bd5f872905d142",
+	},
+	{
+		author: "lsakunes",
+		message: "ewfaoijwef",
+		link: "https://github.com/HarryAllen1/webmaster/commit/3c32b6651863f58b806ea9d158165cb3123c9b10",
+	},
+	{
+		author: "lsakunes",
+		message: "Merge branch 'main' of https://github.com/HarryAllen1/webmaster",
+		link: "https://github.com/HarryAllen1/webmaster/commit/e8595588648d3eabc56721ad614105064d778a25",
+	},
+	{
+		author: "lsakunes",
+		message: "message",
+		link: "https://github.com/HarryAllen1/webmaster/commit/c3828d99bc8475d5a8423d5416c6fff15122d52b",
+	},
+	{
+		author: "lsakunes",
+		message: "stuff",
+		link: "https://github.com/HarryAllen1/webmaster/commit/b85f714785f1e28c826b92f379ce7488ed346b11",
+	},
+	{
+		author: "HarryAllen1",
+		message: "add some bad css",
+		link: "https://github.com/HarryAllen1/webmaster/commit/dc2b9d680664d8fc54cf185548818a3a77e315af",
+	},
+	{
+		author: "HarryAllen1",
+		message: "Merge branch 'main' of https://github.com/HarryAllen1/webmaster",
+		link: "https://github.com/HarryAllen1/webmaster/commit/68aa41eab3c43039ec2837614a418fa091261d8a",
+	},
+	{
+		author: "HarryAllen1",
+		message: "import maps, actually change url",
+		link: "https://github.com/HarryAllen1/webmaster/commit/5994a4f10b72359971fa2cc6651572b23b7b0ea8",
+	},
+	{
+		author: "lsakunes",
+		message: "added a sketchfab model for testing",
+		link: "https://github.com/HarryAllen1/webmaster/commit/972d0125d877666a7a30ec2aa32479e150f5e4a2",
+	},
+	{
+		author: "HarryAllen1",
+		message: "add modesl page",
+		link: "https://github.com/HarryAllen1/webmaster/commit/5e08c6f11e45aaccd45549ffda65b0790dcb6336",
+	},
+	{
+		author: "HarryAllen1",
+		message: "add template file",
+		link: "https://github.com/HarryAllen1/webmaster/commit/11146584228318022a8b6d2af93b141eb096acca",
+	},
+	{
+		author: "HarryAllen1",
+		message: "use components",
+		link: "https://github.com/HarryAllen1/webmaster/commit/ed3c14a7b6cf6c0421f51c71d74072c2e097fcc4",
+	},
+	{
+		author: "HarryAllen1",
+		message: "delete from existence",
+		link: "https://github.com/HarryAllen1/webmaster/commit/63c8ee37f7900b8528e866fdccdd2e5f03ff8a06",
+	},
+	{
+		author: "HarryAllen1",
+		message: "fix errors",
+		link: "https://github.com/HarryAllen1/webmaster/commit/261f2b9f3da748592bf89b3ae86d640a3be91413",
+	},
+	{
+		author: "HarryAllen1",
+		message: "typecheck everything",
+		link: "https://github.com/HarryAllen1/webmaster/commit/30b5447fc2cd343da1202f6310b72b7d27152e63",
+	},
+	{
+		author: "HarryAllen1",
+		message: "strictly type cache",
+		link: "https://github.com/HarryAllen1/webmaster/commit/9a05d7444f4b9b1c1926b67f4f258d6a5a0d6775",
+	},
+	{
+		author: "HarryAllen1",
+		message: "change title on navigate",
+		link: "https://github.com/HarryAllen1/webmaster/commit/c109201b123adb1d492cc9844156e6d33c649494",
+	},
+	{
+		author: "HarryAllen1",
+		message: "add a stupid router",
+		link: "https://github.com/HarryAllen1/webmaster/commit/ec67f3ac884d15704f1a65a08635aae6708158fe",
+	},
+	{
+		author: "HarryAllen1",
+		message: "loader and stuff",
+		link: "https://github.com/HarryAllen1/webmaster/commit/e632a17e3f78784ae238cc6e0e77d8b223c6e5a2",
+	},
+	{
+		author: "HarryAllen1",
+		message: "Use alpine more",
+		link: "https://github.com/HarryAllen1/webmaster/commit/6bc34649e2a25f02f17c2924dc147615f5783d9e",
+	},
+	{
+		author: "HarryAllen1",
+		message: "add link",
+		link: "https://github.com/HarryAllen1/webmaster/commit/de11476bab0b2e4f5b115e08c6588e7daf0f308f",
+	},
+	{
+		author: "HarryAllen1",
+		message: "add a reccomendation",
+		link: "https://github.com/HarryAllen1/webmaster/commit/7230f59b4654cd8fccc2792b8437e3bde335a7af",
+	},
+	{
+		author: "HarryAllen1",
+		message: "dark",
+		link: "https://github.com/HarryAllen1/webmaster/commit/40b9751f5d5576a986d7ae7ae2a466692b26cf35",
+	},
+	{
+		author: "HarryAllen1",
+		message: "use custom eleemnts",
+		link: "https://github.com/HarryAllen1/webmaster/commit/9d86c06da2632021122ee2ce7f2b31df6970df0d",
+	},
+	{
+		author: "HarryAllen1",
+		message: "DRY",
+		link: "https://github.com/HarryAllen1/webmaster/commit/c2a1cea3340257129d19959ff8ede3dc08234343",
+	},
+	{
+		author: "HarryAllen1",
+		message: "add some styling stuff",
+		link: "https://github.com/HarryAllen1/webmaster/commit/5a8f447092f0506296fc95878f010ce83887f41a",
+	},
+	{
+		author: "HarryAllen1",
+		message: "alpine is more suited for this",
+		link: "https://github.com/HarryAllen1/webmaster/commit/45b47181943ff8f33ba0ae08a9ac559cd54cdf22",
+	},
+	{
+		author: "HarryAllen1",
+		message: "initial stuff",
+		link: "https://github.com/HarryAllen1/webmaster/commit/cb4410f01cde9f7653003c97cb6a347951a8f05c",
+	},
+	{
+		author: "HarryAllen1",
+		message: "Initial commit",
+		link: "https://github.com/HarryAllen1/webmaster/commit/35ad5755db037e501978a8ed302db9bb231301c6",
+	},
+];
--- js/index.mjs
+++ js/index.mjs
@@ -1,29 +1,31 @@
 import {
-  createApp,
-  reactive,
-} from 'https://unpkg.com/petite-vue@0.4.1/dist/petite-vue.es.js?module';
+	createApp,
+	reactive,
+} from "https://unpkg.com/petite-vue@0.4.1/dist/petite-vue.es.js?module";
 
 const pages = [
-  ['Home', '/'],
-  ['About', '/about'],
-  ['Models', '/models'],
+	["Home", "/"],
+	["About", "/about"],
+	["Models", "/models"],
 ];
 
 const pageStore = reactive({
-  current: location.pathname.replaceAll('/', ''),
-  /**
-   * @param {string} pathname
-   */
-  updatePage(pathname) {
-    this.current =
-      pathname.replaceAll('/', '') ?? location.pathname.replaceAll('/', '');
-  },
+	current: location.pathname.replaceAll("/", ""),
+	/**
+	 * @param {string} pathname
+	 */
+	updatePage(pathname) {
+		this.current =
+			pathname.replaceAll("/", "") ?? location.pathname.replaceAll("/", "");
+	},
 });
 
-createApp({}).mount('#main');
+createApp({
+	commits: (await import("../about/work-log.mjs")).default,
+}).mount("#main");
 
-if (location.href.includes('models')) {
-  await import('../models/renderer.mjs');
+if (location.href.includes("models")) {
+	await import("../models/renderer.mjs");
 }
 
 /**
@@ -37,77 +39,77 @@ const cachedPages = new Map();
  * @param {Document} newPage
  */
 const updatePage = (newPage) => {
-  /** @type {HTMLDivElement | null} */
-  const main = document.querySelector('#main');
-  if (main)
-    main.innerHTML =
-      newPage.querySelector('#main')?.innerHTML ??
-      'No page found! Try refreshing the page.';
-  document.title = newPage.title;
+	/** @type {HTMLDivElement | null} */
+	const main = document.querySelector("#main");
+	if (main)
+		main.innerHTML =
+			newPage.querySelector("#main")?.innerHTML ??
+			"No page found! Try refreshing the page.";
+	document.title = newPage.title;
 };
 
 const initRouter = () => {
-  document.querySelectorAll('a').forEach((el) => {
-    if (el.href.startsWith(location.origin)) {
-      el.addEventListener('pointerover', async (e) => {
-        e.preventDefault();
-        if (cachedPages.has(el.href)) {
-          return;
-        }
-        const res = await fetch(el.href);
-        const html = await res.text();
-        const parser = new DOMParser();
-        const newPage = parser.parseFromString(html, 'text/html');
-        cachedPages.set(el.href, newPage);
-      });
-      el.addEventListener('click', async (e) => {
-        e.preventDefault();
-        if (
-          new URL(el.href).pathname.replaceAll('/', '') ===
-          new URL(location.href).pathname.replaceAll('/', '')
-        )
-          return;
+	document.querySelectorAll("a").forEach((el) => {
+		if (el.href.startsWith(location.origin)) {
+			el.addEventListener("pointerover", async (e) => {
+				e.preventDefault();
+				if (cachedPages.has(el.href)) {
+					return;
+				}
+				const res = await fetch(el.href);
+				const html = await res.text();
+				const parser = new DOMParser();
+				const newPage = parser.parseFromString(html, "text/html");
+				cachedPages.set(el.href, newPage);
+			});
+			el.addEventListener("click", async (e) => {
+				e.preventDefault();
+				if (
+					new URL(el.href).pathname.replaceAll("/", "") ===
+					new URL(location.href).pathname.replaceAll("/", "")
+				)
+					return;
 
-        if (el.href.includes('models')) {
-          await import('../models/renderer.mjs');
-        }
-        if (cachedPages.has(el.href)) {
-          updatePage(
-            // @ts-ignore: we checked it exists
-            cachedPages.get(el.href)
-          );
-          history.pushState({}, '', el.href);
-          return pageStore.updatePage(new URL(el.href).pathname);
-        }
-        const res = await fetch(el.href);
-        const html = await res.text();
-        const parser = new DOMParser();
-        const newPage = parser.parseFromString(html, 'text/html');
-        cachedPages.set(el.href, newPage);
-        history.pushState({}, '', el.href);
-        updatePage(newPage);
-        pageStore.updatePage(new URL(el.href).pathname);
-      });
-    }
-  });
+				if (el.href.includes("models")) {
+					await import("../models/renderer.mjs");
+				}
+				if (cachedPages.has(el.href)) {
+					updatePage(
+						// @ts-ignore: we checked it exists
+						cachedPages.get(el.href)
+					);
+					history.pushState({}, "", el.href);
+					return pageStore.updatePage(new URL(el.href).pathname);
+				}
+				const res = await fetch(el.href);
+				const html = await res.text();
+				const parser = new DOMParser();
+				const newPage = parser.parseFromString(html, "text/html");
+				cachedPages.set(el.href, newPage);
+				history.pushState({}, "", el.href);
+				updatePage(newPage);
+				pageStore.updatePage(new URL(el.href).pathname);
+			});
+		}
+	});
 };
 
 customElements.define(
-  'wm-navbar',
-  class extends HTMLElement {
-    constructor() {
-      super();
-      this._render();
-    }
-    async _render() {
-      const res = await fetch('/components/navbar.html');
-      const html = await res.text();
-      this.innerHTML = html;
-      createApp({
-        pages,
-        pageStore,
-      }).mount(this);
-      initRouter();
-    }
-  }
+	"wm-navbar",
+	class extends HTMLElement {
+		constructor() {
+			super();
+			this._render();
+		}
+		async _render() {
+			const res = await fetch("/components/navbar.html");
+			const html = await res.text();
+			this.innerHTML = html;
+			createApp({
+				pages,
+				pageStore,
+			}).mount(this);
+			initRouter();
+		}
+	}
 );
