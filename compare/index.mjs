f5227d68fe4197e9361635dd54dc634c790aea79
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-03-13 23:07:24 -0700

    add compare page

--- compare/index.mjs
+++ compare/index.mjs
@@ -1,13 +1,35 @@
 import { createApp, reactive } from 'https://esm.sh/petite-vue@0.4.1';
 import { plans } from '../js/plans.mjs';
+import { toStartCase } from '../js/utils.mjs';
+
+const formatter = new Intl.NumberFormat('en-US', {
+	currency: 'USD',
+	style: 'currency',
+});
 
 const selectedPlan = reactive([plans[0], plans[1], plans[2]]);
 
 const app = createApp({
 	plans,
 	selectedPlan,
+	formatter,
+	toStartCase,
 });
 app.mount('#main');
 document.addEventListener('page-change', () => {
 	app.unmount();
 });
+customElements.define(
+	'wm-divider',
+	class extends HTMLElement {
+		constructor() {
+			super();
+			this.innerHTML = `
+				<span class="flex flex-row items-center my-4">
+					<div class="flex-1 h-[1px] bg-gray"></div>
+					<p class="mx-4 mb-0">${this.getAttribute('label')}</p>
+					<div class="flex-1 h-[1px] bg-gray"></div>
+				</span>`;
+		}
+	},
+);
--- js/utils.mjs
+++ js/utils.mjs
@@ -3,3 +3,9 @@
  * @returns A promise that resolves after ms
  */
 export const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+/**
+ * @param {string} str
+ */
+export const toStartCase = (str) =>
+	str.replace(/([A-Z]+)/g, ' $1').replace(/([A-Z][a-z])/g, ' $1');
