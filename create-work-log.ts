d8919a94a4e71b886321aaadcacac7b14e6c60a6
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-03-13 17:00:51 -0700

    Fix building

--- create-work-log.ts
+++ create-work-log.ts
@@ -4,7 +4,7 @@ import { format } from 'npm:prettier';
 import { CommitData } from './types.d.ts';
 import { AUTHOR_MAP, COMMIT_MAP } from './commit-map.ts';
 
-const ghToken = Deno.env.get('GITHUB_TOKEN');
+const ghToken = Deno.env.get('REPO_TOKEN') ?? Deno.env.get('GITHUB_TOKEN');
 const outFile = './about/work-log.mjs';
 
 const res = await fetch(
@@ -15,7 +15,7 @@ const res = await fetch(
 			Authorization: `Bearer ${ghToken}`,
 			'X-GitHub-Api-Version': '2022-11-28',
 		},
-	},
+	}
 );
 
 const commits: CommitData[] = await res.json();
@@ -36,31 +36,29 @@ while (res.headers.get('link')?.includes('rel="next"')) {
 Deno.writeTextFile(
 	outFile,
 	format(
-		`export default ${
-			JSON.stringify(
-				commits
-					.filter((i) => !i.commit.message.includes('Merge branch'))
-					.map((c) => ({
-						author: c.author.login,
-						message: c.commit.message,
-						link: c.html_url,
-						date: c.commit.author.date,
-						hash: c.sha,
-					}))
-					.map((c) => {
-						if (COMMIT_MAP[c.hash]) {
-							c.message = COMMIT_MAP[c.hash];
-						}
-						if (AUTHOR_MAP[c.author]) {
-							c.author = AUTHOR_MAP[c.author];
-						}
-						return c;
-					}),
-			)
-		}`,
+		`export default ${JSON.stringify(
+			commits
+				.filter((i) => !i.commit.message.includes('Merge branch'))
+				.map((c) => ({
+					author: c.author.login,
+					message: c.commit.message,
+					link: c.html_url,
+					date: c.commit.author.date,
+					hash: c.sha,
+				}))
+				.map((c) => {
+					if (COMMIT_MAP[c.hash]) {
+						c.message = COMMIT_MAP[c.hash];
+					}
+					if (AUTHOR_MAP[c.author]) {
+						c.author = AUTHOR_MAP[c.author];
+					}
+					return c;
+				})
+		)}`,
 		{
 			...JSON.parse(await Deno.readTextFile('./.prettierrc')),
 			parser: 'babel',
-		},
-	),
+		}
+	)
 );
