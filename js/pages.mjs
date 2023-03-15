13a1c86d8465d6c22182bebae494f11f38f3bcc7
StudentUser101 <122959554+StudentUser101@users.noreply.github.com>
2023-03-14 23:53:28 -0700

    Second iteration of 'Trip' page. Worked on visual look (didn't change content). Also removed old 'Models' page

--- js/pages.mjs
+++ js/pages.mjs
@@ -2,7 +2,6 @@ export const pages = [
 	['Home', '/'],
 	['Book', '/book'],
 	['Compare', '/compare'],
-	['Models', '/models'],
 	['Trip', '/launchinfo'],
 	['Vehicles', '/vehicles'],
 	['About', '/about'],
