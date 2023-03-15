ca628e8336eaf3ffd6cdb76e8812815e63c6a914
lsakunes <senukasl@outlook.com>
2023-03-15 16:14:00 -0700

    working on models

--- vehicles/index.mjs
+++ vehicles/index.mjs
@@ -10,14 +10,23 @@ import {
 	WebGLRenderer,
 } from 'https://esm.sh/three@0.150.1/build/three.module.js';
 
+import{
+	GLTFLoader,
+} from 'https://esm.sh/three/examples/jsm/loaders/GLTFLoader.js';
+
+import { OrbitControls } from 'https://esm.sh/three/examples/jsm/controls/OrbitControls.js';
+
 const scene = new Scene();
 
-// Add a cube to the scene
-const geometry = new BoxGeometry(3, 1, 3); // width, height, depth
-const material = new MeshLambertMaterial({ color: 0xfb8e00 });
-const mesh = new Mesh(geometry, material);
-mesh.position.set(0, 0, 0);
-scene.add(mesh);
+//get gltf from assets/models/adante.gltf
+const loader = new GLTFLoader();
+loader.load('../../assets/models/adante.gltf', (gltf) => {
+	console.log(gltf);
+	const model = gltf.scene;
+	model.scale.set(0.1, 0.1, 0.1);
+	model.position.set(0, 0, 0);
+	scene.add(model);
+});
 
 // Set up lights
 const ambientLight = new AmbientLight(0xffffff, 0.6);
@@ -37,9 +46,22 @@ camera.lookAt(0, 0, 0);
 
 camera.updateProjectionMatrix();
 
+
 // Renderer
 const renderer = new WebGLRenderer({ antialias: true });
+//renderer.setClearColor( 0xffffff, 0);
 renderer.setSize(window.innerWidth * 0.5, window.innerHeight);
+
+
+// Set up controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.screenSpacePanning = false;
+controls.minDistance = 1;
+controls.maxDistance = 500;
+controls.maxPolarAngle = Math.PI / 2;
+
 renderer.render(scene, camera);
 renderer.domElement.classList.add('max-w-6xl', 'z-10');
 document.querySelector('#main')?.appendChild(renderer.domElement);
