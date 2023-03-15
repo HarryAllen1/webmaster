ce7a364152d1bb05b14f24564031967ad18a2182
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-03-14 17:49:21 -0700

    Remove work log

--- about/index.mjs
+++ about/index.mjs
@@ -1,8 +1,6 @@
 import { createApp } from 'https://esm.sh/petite-vue@0.4.1';
 
-const app = createApp({
-	commits: (await import('./work-log.mjs')).default,
-});
+const app = createApp({});
 app.mount('#main');
 document.addEventListener('page-change', () => {
 	app.unmount();
