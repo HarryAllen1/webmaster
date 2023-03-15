e8bbe801dcb414a6116b1efb0160e55dda349ba6
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-03-15 10:27:43 -0700

    Fix adding of too many items to cart

--- book/index.mjs
+++ book/index.mjs
@@ -1,6 +1,7 @@
 import { createApp } from 'https://esm.sh/petite-vue@0.4.1';
 import Toastify from 'https://esm.sh/toastify-js@1.12.0';
 import { CART_KEY } from '../js/constants.mjs';
+import { plans } from '../js/plans.mjs';
 
 const app = createApp({
 	toastMessage: '',
@@ -15,12 +16,35 @@ const app = createApp({
 		 */
 		// @ts-ignore: javascript moment
 		const target = e.target;
+		const name =
+			target.parentElement?.parentElement?.querySelector('#name')
+				?.textContent ?? '';
+
+		const productIndex = currentCart.findIndex((v) => v[0] === name);
+		if (productIndex === -1) {
+			currentCart.push([name, 1]);
+		} else {
+			if (
+				currentCart[productIndex][1] >=
+				plans.find((i) => i.name === name).maxPeople
+			) {
+				return Toastify({
+					text: `Max number of ${name} bookings reached.`,
+					style: {
+						background: 'var(--bs-danger)',
+					},
+					position: 'center',
+					gravity: 'bottom',
+					close: true,
+				}).showToast();
+			}
+			currentCart[productIndex][1]++;
+		}
 		Toastify({
-			text: `Added 1 ${target.parentElement?.parentElement?.querySelector(
-				'#name',
-			)?.textContent} flight to cart for ${target.parentElement?.parentElement
-				?.querySelector('#price')
-				?.textContent}`,
+			text: `Added 1 ${name} flight to cart for ${
+				target.parentElement?.parentElement?.querySelector('#price')
+					?.textContent
+			}`,
 			style: {
 				background: 'var(--bs-blue)',
 			},
@@ -28,20 +52,13 @@ const app = createApp({
 			gravity: 'bottom',
 			close: true,
 		}).showToast();
-		const name = target.parentElement?.parentElement?.querySelector('#name')
-			?.textContent ?? '';
-		const productIndex = currentCart.findIndex((v) => v[0] === name);
-		if (productIndex === -1) {
-			currentCart.push([name, 1]);
-		} else {
-			currentCart[productIndex][1]++;
-		}
 		localStorage.setItem(CART_KEY, JSON.stringify(currentCart));
 		const event = new CustomEvent('add-to-cart', {
 			detail: {
 				count: currentCart.reduce((acc, v) => acc + v[1], 0),
 			},
 		});
+
 		document.dispatchEvent(event);
 	},
 });
