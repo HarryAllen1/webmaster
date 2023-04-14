77a15cbf77a0466ee1b85b7af1956534fdee6340
lsakunes <senukasl@outlook.com>
2023-04-14 11:47:42 -0700

    did some cool js shit

--- vehicles/andante.mjs
+++ vehicles/andante.mjs
@@ -6,6 +6,10 @@ import {
 	Scene,
 	Vector3,
 	WebGLRenderer,
+	PointLight,
+	Raycaster,
+	Vector2,
+	Plane,
 } from 'https://esm.sh/three@0.151.3?bundle';
 import { OrbitControls } from 'https://esm.sh/three@0.151.3/examples/jsm/controls/OrbitControls.js?bundle';
 import { GLTFLoader } from 'https://esm.sh/three@0.151.3/examples/jsm/loaders/GLTFLoader.js?bundle';
@@ -15,64 +19,38 @@ customElements.define(
 	class extends HTMLElement {
 		constructor() {
 			super();
-
 			this.classList.add('w-full');
-
 			const renderer = new WebGLRenderer();
-
-			// set the size of the renderer
 			renderer.setSize(this.getBoundingClientRect().width, window.innerHeight);
-
-			// create the scene
 			const scene = new Scene();
-
-			// create the camera
 			const camera = new PerspectiveCamera(
 				75,
 				window.innerWidth / window.innerHeight,
 				0.1,
 				1000
 			);
-
-			// create the controls
 			const controls = new OrbitControls(camera, renderer.domElement);
-
 			controls.enableZoom = false;
-
-			//start the camera a bit further away
 			camera.position.z = 5;
-
-			// set the camera position
-			camera.position.z = 5;
-
-			// create the ambient light but make it really strong
-			const ambientLight = new AmbientLight(0xffffff, 1);
+			const ambientLight = new AmbientLight(0xffffff, 5);
 			scene.add(ambientLight);
-
-			// create the directional light but make it really strong
 			const directionalLight = new DirectionalLight(0xffffff, 1);
+			directionalLight.position.set(0, 0, 1);
 			scene.add(directionalLight);
-
-			// load the adante model
+			const directionalLight2 = new DirectionalLight(0xffffff, 1);
+			directionalLight2.position.set(0, 0, -1);
+			scene.add(directionalLight2);
 			const loader = new GLTFLoader();
-
 			loader.load(
 				'../../assets/models/adante/adante.gltf',
 				(gltf) => {
 					const object = gltf.scene;
-					//scale down the model a lot
 					object.scale.set(0.001, 0.001, 0.001);
-					//center the model
 					const box = new Box3().setFromObject(object);
-
-					// deno-lint-ignore no-unused-vars
-					const boxSize = box.getSize(new Vector3()).length();
 					const boxCenter = box.getCenter(new Vector3());
 					object.position.x = -boxCenter.x;
 					object.position.y = -boxCenter.y;
 					object.position.z = -boxCenter.z;
-
-					//add the model to the scene
 					scene.add(object);
 				},
 				(xhr) => {
@@ -80,18 +58,63 @@ customElements.define(
 				},
 				console.error
 			);
+			
+			const raycaster = new Raycaster();
+			const mouse = new Vector2();
+			const intersectPoint = new Vector3();
+			
+			const plane = new Plane(new Vector3(0, 0, 0), 0);
+			
+			function updatePlane(camera) {
+				
+				const forward = new Vector3();
+				camera.getWorldDirection(forward);
+				
+				plane.normal.copy(forward);
+				
+				const cameraPos = new Vector3();
+				camera.getWorldPosition(cameraPos);
+				plane.constant = cameraPos.dot(forward);
+			}
+			
+			function onMouseMove(event) {	
+				const rect = renderer.domElement.getBoundingClientRect();
+				mouse.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+				mouse.y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
+			}
+			
+			function updateIntersectPoint(camera, model) {
+				updatePlane(camera);
+				raycaster.setFromCamera(mouse, camera);
+				const modelIntersects = raycaster.intersectObject(model, true);
+				if (modelIntersects.length > 0) {
+					intersectPoint.copy(modelIntersects[0].point);
+				} else {
+					
+					raycaster.ray.intersectPlane(plane, intersectPoint);
+				}
+			}
+			
+			addEventListener('mousemove', onMouseMove, false);
+			addEventListener('touchmove', onMouseMove, false);
 
-			// render the scene
+			const pointLight = new PointLight(0x0f00ff, 20, 10);
+			
+			function animate() {
+				requestAnimationFrame(animate);
+				updateIntersectPoint(camera, scene);
+				updatePlane(camera);
+				pointLight.position.copy(intersectPoint);
+			}
+			animate();
+			scene.add(pointLight);
+
 			const render = () => {
 				requestAnimationFrame(render);
 				renderer.render(scene, camera);
 			};
 			render();
-
-			// make the background transparent
 			renderer.setClearColor(0x000000, 0);
-
-			// add the renderer to the DOM
 			renderer.domElement.classList.add('!w-full', 'z-10');
 			this.innerHTML = '';
 			this.append(renderer.domElement);
