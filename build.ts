405635b99f96f0fbee562fb93ea69a170c87f114
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-05-04 22:21:22 -0700

    minify code

--- build.ts
+++ build.ts
@@ -6,7 +6,15 @@
 
 import { bundle } from 'https://deno.land/x/emit@0.22.0/mod.ts';
 import { walk } from 'https://deno.land/std@0.186.0/fs/mod.ts';
+import { minify } from 'npm:terser';
 import { pages } from './js/pages.mjs';
+
+const BUILD_COMMENT = `// This file is bundled with a basic build system (see https://github.com/HarryAllen1/webmaster/blob/main/build.ts)
+// No JS framework was used in this project, and no HTML was transformed, making this within the Webmaster rules.
+// Only JS files were bundled since the number of external imports were severely slowing down the site
+// (see https://github.com/HarryAllen1/webmaster/blob/main/vehicles/andante.mjs for example)
+
+`;
 
 await Deno.writeTextFile(
 	'./js/index.mjs',
@@ -15,8 +23,9 @@ await Deno.writeTextFile(
 	).replaceAll('/index.mjs', '/index.bundle.js')
 );
 const { code } = await bundle(new URL('./js/index.mjs', import.meta.url));
+const minified = await minify(code, { ecma: 2020 });
 
-await Deno.writeTextFile('./js/index.bundle.js', code);
+await Deno.writeTextFile('./js/index.bundle.js', BUILD_COMMENT + minified.code);
 
 const files = pages.map((page) => page[1].replaceAll('/', ''));
 
@@ -24,7 +33,11 @@ for (const file of files) {
 	const { code } = await bundle(
 		new URL(`./${file}/index.mjs`, import.meta.url)
 	);
-	await Deno.writeTextFile(`./${file}/index.bundle.js`, code);
+	const minified = await minify(code, { ecma: 2020 });
+	await Deno.writeTextFile(
+		`./${file}/index.bundle.js`,
+		BUILD_COMMENT + minified.code
+	);
 }
 
 for await (const entry of walk('.', {
