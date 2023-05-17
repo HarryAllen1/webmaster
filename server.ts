63fd2689c7e880574254a08d424bb89c2bb176a4
Harry Allen <66224939+HarryAllen1@users.noreply.github.com>
2023-05-17 16:37:24 +0000

    ignore some files

--- server.ts
+++ server.ts
@@ -1,11 +1,15 @@
 // a simple server to staticly serve all files. For hosting purposes.
-// @ts-nocheck - This is the only deno file
 import { serveDir } from 'https://deno.land/std@0.186.0/http/file_server.ts';
 import { serve } from 'https://deno.land/std@0.186.0/http/server.ts';
 
 serve(async (req) => {
 	const res = await serveDir(req);
-	if (res.status === 404) {
+	const url = new URL(req.url);
+	if (
+		res.status === 404 ||
+		url.pathname.startsWith('/.') ||
+		url.pathname.endsWith('.json')
+	) {
 		const file = await Deno.readFile('./404.html');
 		return new Response(file, {
 			status: 404,
