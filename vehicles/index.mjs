65f1d09eb82ec1e516f3aeeaec79bbe40c84fdf7
Senuka Liyanage <senukasl@outlook.com>
2023-05-20 14:07:58 -0700

    added a cooler effect for the vehicles

--- vehicles/index.mjs
+++ vehicles/index.mjs
@@ -30,28 +30,26 @@ customElements.define(
 				0.1,
 				1000
 			);
-			const controls = new OrbitControls(camera, renderer.domElement);
-			controls.enableZoom = false;
 			camera.position.z = 5;
-			const ambientLight = new AmbientLight(0xffffff, 5);
+			const ambientLight = new AmbientLight(0xffffff, 0.01);
 			scene.add(ambientLight);
-			const directionalLight = new DirectionalLight(0xfff9ff, 1);
-			directionalLight.position.set(0, 0, 1);
+			const directionalLight = new DirectionalLight(0xfff9ff, 0.3);
+			directionalLight.position.set(1, 0, 0);
 			scene.add(directionalLight);
-			const directionalLight2 = new DirectionalLight(0xfffff9, 1);
-			directionalLight2.position.set(0, 0, -1);
-			scene.add(directionalLight2);
 			const loader = new GLTFLoader();
+			var standardRotation;
+			var object;
 			loader.load(
 				this.dataset.filepath ?? '',
 				(gltf) => {
-					const object = gltf.scene;
+					object = gltf.scene;
 					object.scale.set(0.001, 0.001, 0.001);
 					const box = new Box3().setFromObject(object);
 					const boxCenter = box.getCenter(new Vector3());
 					object.position.x = -boxCenter.x;
 					object.position.y = -boxCenter.y;
 					object.position.z = -boxCenter.z;
+					standardRotation = object.rotation;
 					//reduce the shine
 					object.traverse(
 						/** @param {any} child */ (child) => {
@@ -68,10 +66,7 @@ customElements.define(
 				},
 				console.error
 			);
-
-			const raycaster = new Raycaster();
 			const mouse = new Vector2();
-			const intersectPoint = new Vector3();
 
 			const plane = new Plane(new Vector3(0, 0, 0), 0);
 
@@ -90,46 +85,31 @@ customElements.define(
 				plane.constant += 5;
 			}
 
-			/**
-			 * @param {PerspectiveCamera} camera
-			 * @param {import('https://esm.sh/three@0.152.2?bundle&exports=AmbientLight,Box3,DirectionalLight,PerspectiveCamera,Plane,PointLight,Raycaster,Scene,Vector2,Vector3,WebGLRenderer,Object3D').Object3D} model
-			 */
-			function updateIntersectPoint(camera, model) {
-				updatePlane(camera);
-				raycaster.setFromCamera(mouse, camera);
-				if (raycaster.params.Line?.threshold) {
-					raycaster.params.Line.threshold = 0.5;
-				}
-				const modelIntersects = raycaster.intersectObject(model, true);
-				if (modelIntersects.length > 0) {
-					intersectPoint.copy(modelIntersects[0].point);
-					const cameraPos = new Vector3();
-					camera.getWorldPosition(cameraPos);
-					const direction = intersectPoint.clone().sub(cameraPos).normalize();
-					intersectPoint.sub(direction.multiplyScalar(0.5));
-				} else {
-					raycaster.ray.intersectPlane(plane, intersectPoint);
-				}
-			}
-
 			globalThis.addEventListener('mousemove', (event) => {
 				const rect = renderer.domElement.getBoundingClientRect();
 				mouse.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
 				mouse.y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
 			});
 
-			const pointLight = new PointLight(0x00ffff, 5, 1);
-			pointLight.distance = 150;
-			pointLight.decay = 100;
+			function updateDirectionalLight(){
+				directionalLight.position.set(mouse.x*4, mouse.y*4, 1);
+			}
+
+			function updateObjRotation(obj){
+				if (obj){
+					console.log(mouse);
+					obj.rotation.x = mouse.y*0.3;
+					obj.rotation.y = -mouse.x*0.3;
+				}
+			}
 
 			function animate() {
 				requestAnimationFrame(animate);
-				updateIntersectPoint(camera, scene);
+				updateDirectionalLight();
+				updateObjRotation(object);
 				updatePlane(camera);
-				pointLight.position.copy(intersectPoint);
 			}
 			animate();
-			//scene.add(pointLight);
 
 			const render = () => {
 				requestAnimationFrame(render);
