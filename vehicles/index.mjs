187ab8a37aad5887e2fa4da3e27debef1b2aa41f
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-05-21 19:41:46 -0700

    fix linting

--- vehicles/index.mjs
+++ vehicles/index.mjs
@@ -90,6 +90,11 @@ customElements.define(
 				directionalLight.position.set(mouse.x * 4, mouse.y * 4, 1);
 			}
 
+			/**
+			 * @param {number} num
+			 * @param {number} min
+			 * @param {number} max
+			 */
 			function clamp(num, min, max) {
 				return num <= min ? min : num >= max ? max : num;
 			}
